2826506c5db84354a29851ad6e5d6ce6bd8d0691
Vikas Mali <vikasmali@2409.gmail.com>
2025-06-10 08:08:50 +0530

    Home Assignment

---
+++ java/day 6/Home Assignment/PositiveNegative.java
@@ -0,0 +1,25 @@
+package Demo;
+import java.util.*;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Number:");
+		int num=s.nextInt();
+		if(num>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("Negative");
+		}
+		else {
+			System.out.println("Zero");
+		}
+	}
+
+}
